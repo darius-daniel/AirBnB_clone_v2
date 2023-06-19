40694ada82a72198e716b165edca634f22fc2a55
Darius Daniel <dariusdaniel771@gmail.com>
2023-06-19 11:36:26 +0100

    Initial commit to repository

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,19 +1,26 @@
 #!/usr/bin/python3
-""" """
+""" A suit of unittests for Amenity objects
+"""
 from tests.test_models.test_base_model import test_basemodel
 from models.amenity import Amenity
 
 
 class test_Amenity(test_basemodel):
-    """ """
+    """
+    Perform tests on the Amenity class
+    """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """
+        Tests on new object instantiation
+        """
         super().__init__(*args, **kwargs)
         self.name = "Amenity"
         self.value = Amenity
 
     def test_name2(self):
-        """ """
+        """
+        Test a new object's name
+        """
         new = self.value()
         self.assertEqual(type(new.name), str)
