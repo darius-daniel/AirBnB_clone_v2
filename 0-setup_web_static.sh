ea2c869fd1001a559079ce0b98af9251ba9a2502
Darius Daniel <dariusdaniel771@gmail.com>
2023-07-05 12:37:04 +0100

    Used sed command to update Nginx configuration to serve content

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -23,4 +23,7 @@ ln -s /data/web_static/releases/test/ /data/web_static/current
 
 chown -R ubuntu:ubuntu /data/
 
+sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
+
 service nginx restart
+exit 0
