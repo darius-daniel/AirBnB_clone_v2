d59e1fbc34c32adc82329d03bcab6430ccb2ffc2
Darius Daniel <dariusdaniel771@gmail.com>
2023-07-22 17:54:24 +0100

    modified State class in models/state.py

--- models/state.py
+++ models/state.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
+from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 import models
+import shlex
 
 
 class State(BaseModel, Base):
@@ -23,7 +25,8 @@ class State(BaseModel, Base):
         matches = []
 
         for key in data:
-            city = key.split('.')
+            city = key.replace('.', ' ')
+            city = shlex.split(city)
             if city[0] == 'City':
                 city_objs.append(data[key])
 
