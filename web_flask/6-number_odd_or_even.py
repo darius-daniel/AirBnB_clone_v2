119a5ba40522887df31966fbe980028f852e537b
Darius Daniel <dariusdaniel771@gmail.com>
2023-07-22 02:30:06 +0100

    Added second blank line after num_temp_text() to match formatting instructions

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -78,5 +78,6 @@ def odd_or_even(n):
             string = 'even'
         return render_template('6-number_odd_or_even.html', number=n, div_by_2=string)
 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
