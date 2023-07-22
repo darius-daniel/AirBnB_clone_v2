db4f99adaa95318c5857beae265317f0a4d3b658
Darius Daniel <dariusdaniel771@gmail.com>
2023-07-22 15:25:46 +0100

    Importing Amenity from models.amenty

--- models/place.py
+++ models/place.py
@@ -4,6 +4,7 @@ import models
 import os
 from models.base_model import BaseModel, Base
 from models.review import Review
+from models.amenity import Amenity
 from sqlalchemy import Column, String, Integer, ForeignKey, Float
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
